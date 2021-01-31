e4d4004ca3511e7654e69d62b6ad4d007ee9c0e0
Sudipto Ghosh <70814428+sudiptoghosh8@users.noreply.github.com>
2021-02-01 01:14:24 +0600

    Add files via upload

---
+++ Add Two Numbers Problem Code FLOW001.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int t,i,n,a,b;
+    cin>>t;
+    for(i=1;i<=t;i++)
+    {
+        cin>>a>>b;
+        n=a+b;
+        cout<<n<<endl;
+    }
+    return 0;
+}
