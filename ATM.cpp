be1b2a0a869910eeb54daab36e22e26bc16294ef
Sudipto Ghosh <70814428+sudiptoghosh8@users.noreply.github.com>
2021-02-01 00:26:29 +0600

    Add files via upload

---
+++ ATM.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a;
+    float b,c,d;
+    cin>>a>>b;
+   if(a%5==0)
+   {
+
+    if(a<b)
+    {
+       c= b-a;
+    d=c-0.5;
+    cout<<d<<endl;
+    }
+
+   }
+   else
+   {cout<<b<<endl;}
+	// your code goes here
+	return 0;
+}
